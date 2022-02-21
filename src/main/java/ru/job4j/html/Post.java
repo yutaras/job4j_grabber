0fadf75f25f76d2cd36ef6f90a9546ba48364115
Yuriy Tarasenko <maxjeff@mail.ru>
2022-02-21 22:18:00 +0300

    2.2. Модель данных - Post.

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -10,6 +10,61 @@ public class Post {
     public String description;
     public LocalDateTime created;
 
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -19,20 +74,20 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description) && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, created);
+        return Objects.hash(id, link);
     }
 
     @Override
     public String toString() {
-        return "Post{"
-                + "title='" + title + '\''
-                + ", link='" + link + '\''
-                + ", description='" + description
-                + '\'' + ", created=" + created + '}';
+        return "Post{" + "id="
+                + id + ", title='"
+                + title + '\'' + ", link='"
+                + link + '\'' + ", description='"
+                + description + '\'' + ", created=" + created + '}';
     }
 }
