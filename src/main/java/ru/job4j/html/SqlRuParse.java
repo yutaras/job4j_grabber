1e9c0591abc2df6fb0dc8a0f03669e3670f1f2bc
Yuriy Tarasenko <maxjeff@mail.ru>
2022-02-21 20:47:20 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #267837]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,14 +7,16 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element time = td.parent().child(5);
-            System.out.println(time.text());
+        for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element time = td.parent().child(5);
+                System.out.println(time.text());
+            }
         }
     }
 }
