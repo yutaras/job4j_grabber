1e45407814109cba868f78279b6cccb3a0d605e7
Yuriy Tarasenko <maxjeff@mail.ru>
2022-02-21 15:36:19 +0300

    2.1. Преобразование даты [#289476 #264230]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,45 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("янв", "1"),
+            Map.entry("фев", "2"),
+            Map.entry("мар", "3"),
+            Map.entry("апр", "4"),
+            Map.entry("май", "5"),
+            Map.entry("июн", "6"),
+            Map.entry("июл", "7"),
+            Map.entry("авг", "8"),
+            Map.entry("сен", "9"),
+            Map.entry("окт", "10"),
+            Map.entry("ноя", "11"),
+            Map.entry("дек", "12")
+    );
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        String[] twoParts = parse.split(",");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime time = LocalTime.parse(twoParts[1].replace(" ", ""), dtf);
+        String[] partsOfDate = twoParts[0].split(" ");
+        LocalDate date = LocalDate.of(2000 + Integer.parseInt(partsOfDate[2]),
+                Integer.parseInt(MONTHS.get(partsOfDate[1])),
+                Integer.parseInt(partsOfDate[0]));
+        LocalDateTime date1 = LocalDateTime.of(date, time);
+        System.out.println(date1);
+        return date1;
+    }
+
+    public static void main(String[] args) {
+        String data = "2 дек 22, 10:50";
+        SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
+        sql.parse(data);
+    }
+}
