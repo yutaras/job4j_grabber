46e7e7e069211844bb647f46894b422c6ce89685
Yuriy Tarasenko <maxjeff@mail.ru>
2022-02-24 14:06:12 +0300

    2.3. Загрузка деталей поста. [#285212 #268706]

--- src/main/java/ru/job4j/html/LoadPost.java
+++ src/main/java/ru/job4j/html/LoadPost.java
@@ -8,15 +8,23 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class LoadPost {
+    /*public Post loadPost(String page) throws IOException {
+        Document doc = Jsoup.connect(page).get();
+        Elements row = doc.select(".msgBody");
+        Element description = row.get(0).parent().child(1);
+        return new Post()
+    }*/
 
     public static void main(String[] args) throws IOException {
         String page =
                 "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
         Document doc = Jsoup.connect(page).get();
         Elements row = doc.select(".msgBody");
-        for (Element td : row) {
-            Element date = td.parent().child(1);
-            System.out.println(date.text());
-        }
+        Element description = row.get(0).parent().child(1);
+        Elements lines = doc.select(".msgFooter");
+        Element created = lines.get(0).parent();
+        String[] date = created.text().split("    ");
+        System.out.println(description.text());
+        System.out.println(date[0]);
     }
 }
