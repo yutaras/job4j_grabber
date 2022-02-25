9f956e42e03acf695c882af747767c72ce167a35
Yuriy Tarasenko <maxjeff@mail.ru>
2022-02-25 16:00:48 +0300

    2.3. Загрузка деталей поста. [#285212 #268706]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -20,19 +20,16 @@ public class SqlRuParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
-    @Override
     public Post detail(String link) throws IOException {
-        SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
         Document doc = Jsoup.connect(link).get();
         String name = doc.select(".messageHeader").get(0).ownText();
         String description = doc.select(".msgBody").get(1).text();
         Element createdE = doc.select(".msgFooter").get(0);
         String[] date = createdE.text().split("    ");
-        LocalDateTime created = sql.parse(date[0]);
+        LocalDateTime created = dateTimeParser.parse(date[0]);
         return new Post(name, link, description, created);
     }
 
-    @Override
     public List<Post> list(String link) throws IOException {
         List<Post> list = new ArrayList<>();
         Document doc = Jsoup.connect(link).get();
@@ -44,13 +41,12 @@ public class SqlRuParse implements Parse {
         return list;
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DateTimeParser parser = new SqlRuDateTimeParser();
+        Parse pars = new SqlRuParse(parser);
         for (int i = 1; i < 6; i++) {
             String link = "https://www.sql.ru/forum/job-offers/";
-
-
+            System.out.println(pars.list(link));
         }
     }
-
 }
