b1c614c68ef071bc9d28ef321ca90064b563162d
Yuriy Tarasenko <maxjeff@mail.ru>
2022-02-26 11:22:33 +0300

    2.4. SqlRuParse [#285213 #269062]

--- src/main/java/ru/job4j/html/Parse.java
+++ src/main/java/ru/job4j/html/Parse.java
@@ -1,10 +1,9 @@
 package ru.job4j.html;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface Parse {
-    List<Post> list(String link) throws IOException;
+    List<Post> list(String link);
 
-    Post detail(String link) throws IOException;
+    Post detail(String link);
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -20,30 +20,42 @@ public class SqlRuParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
-    public Post detail(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
-        String name = doc.select(".messageHeader").get(0).ownText();
-        String description = doc.select(".msgBody").get(1).text();
-        Element createdE = doc.select(".msgFooter").get(0);
-        String[] date = createdE.text().split("    ");
-        LocalDateTime created = dateTimeParser.parse(date[0]);
+    public Post detail(String link) {
+        String name = null;
+        String description = null;
+        LocalDateTime created = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            name = doc.select(".messageHeader").get(0).ownText();
+            description = doc.select(".msgBody").get(1).text();
+            Element createdE = doc.select(".msgFooter").get(0);
+            String[] date = createdE.text().split("    ");
+            created = dateTimeParser.parse(date[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Post(name, link, description, created);
     }
 
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> list = new ArrayList<>();
-        Document doc = Jsoup.connect(link).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            if (href.text().contains("Java") && !href.text().contains("Javascript")) {
-                list.add(detail(href.attr("href")));
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                if (href.text().toLowerCase().contains("java")
+                        && !href.text().toLowerCase().contains("javascript")) {
+                    list.add(detail(href.attr("href")));
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return list;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         DateTimeParser parser = new SqlRuDateTimeParser();
         Parse pars = new SqlRuParse(parser);
         for (int i = 1; i < 6; i++) {
